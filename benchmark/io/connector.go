127fed005dbb9e8e65704bdd262d76a77ed32d5a
GuiRongLiu <hroomkeeper@gmail.com>
2024-08-20 15:55:28 +0800

    doc by copilot

--- benchmark/io/connector.go
+++ benchmark/io/connector.go
@@ -1,22 +1,10 @@
-// Copyright (c) nano Authors. All Rights Reserved.
+// nano 作者版权所有。保留所有权利。
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// 特此免费授予任何获得本软件及相关文档文件（“软件”）副本的人员使用、复制、修改、合并、发布、分发、再许可和/或出售软件副本的权限，并允许向其提供软件的人员这样做，但须符合以下条件：
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// 上述版权声明和本许可声明应包含在软件的所有副本或主要部分中。
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// 本软件按“原样”提供，不提供任何明示或暗示的担保，包括但不限于对适销性、特定用途适用性和非侵权的担保。在任何情况下，作者或版权持有人均不对因本软件或本软件的使用或其他交易而产生的任何索赔、损害或其他责任负责，无论是在合同诉讼、侵权诉讼或其他诉讼中。
 
 package io
 
@@ -32,8 +20,8 @@ import (
 )
 
 var (
-	hsd []byte // handshake data
-	had []byte // handshake ack data
+	hsd []byte // 握手数据
+	had []byte // 握手确认数据
 )
 
 func init() {
@@ -50,32 +38,30 @@ func init() {
 }
 
 type (
-
-	// Callback represents the callback type which will be called
-	// when the correspond events is occurred.
+	// Callback 表示在相应事件发生时将被调用的回调类型
 	Callback func(data interface{})
 
-	// Connector is a tiny Nano client
+	// Connector 是一个小型的 Nano 客户端
 	Connector struct {
-		conn   net.Conn       // low-level connection
-		codec  *codec.Decoder // decoder
-		die    chan struct{}  // connector close channel
-		chSend chan []byte    // send queue
-		mid    uint64         // message id
+		conn   net.Conn       // 底层连接
+		codec  *codec.Decoder // 解码器
+		die    chan struct{}  // 连接关闭通道
+		chSend chan []byte    // 发送队列
+		mid    uint64         // 消息ID
 
-		// events handler
+		// 事件处理器
 		muEvents sync.RWMutex
 		events   map[string]Callback
 
-		// response handler
+		// 响应处理器
 		muResponses sync.RWMutex
 		responses   map[uint64]Callback
 
-		connectedCallback func() // connected callback
+		connectedCallback func() // 连接成功回调
 	}
 )
 
-// NewConnector create a new Connector
+// NewConnector 创建一个新的 Connector 实例
 func NewConnector() *Connector {
 	return &Connector{
 		die:       make(chan struct{}),
@@ -87,7 +73,7 @@ func NewConnector() *Connector {
 	}
 }
 
-// Start connect to the server and send/recv between the c/s
+// Start 连接到服务器并在客户端和服务器之间发送/接收数据
 func (c *Connector) Start(addr string) error {
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
@@ -98,21 +84,21 @@ func (c *Connector) Start(addr string) error {
 
 	go c.write()
 
-	// send handshake packet
+	// 发送握手数据包
 	c.send(hsd)
 
-	// read and process network message
+	// 读取并处理网络消息
 	go c.read()
 
 	return nil
 }
 
-// OnConnected set the callback which will be called when the client connected to the server
+// OnConnected 设置在客户端连接到服务器时将被调用的回调函数
 func (c *Connector) OnConnected(callback func()) {
 	c.connectedCallback = callback
 }
 
-// Request send a request to server and register a callbck for the response
+// Request 发送请求到服务器并为响应注册回调
 func (c *Connector) Request(route string, v proto.Message, callback Callback) error {
 	data, err := serialize(v)
 	if err != nil {
@@ -135,7 +121,7 @@ func (c *Connector) Request(route string, v proto.Message, callback Callback) er
 	return nil
 }
 
-// Notify send a notification to server
+// Notify 发送通知到服务器
 func (c *Connector) Notify(route string, v proto.Message) error {
 	data, err := serialize(v)
 	if err != nil {
@@ -150,7 +136,7 @@ func (c *Connector) Notify(route string, v proto.Message) error {
 	return c.sendMessage(msg)
 }
 
-// On add the callback for the event
+// On 为事件添加回调
 func (c *Connector) On(event string, callback Callback) {
 	c.muEvents.Lock()
 	defer c.muEvents.Unlock()
@@ -158,12 +144,13 @@ func (c *Connector) On(event string, callback Callback) {
 	c.events[event] = callback
 }
 
-// Close close the connection, and shutdown the benchmark
+// Close 关闭连接并关闭基准测试
 func (c *Connector) Close() {
 	c.conn.Close()
 	close(c.die)
 }
 
+// eventHandler 获取事件处理器
 func (c *Connector) eventHandler(event string) (Callback, bool) {
 	c.muEvents.RLock()
 	defer c.muEvents.RUnlock()
@@ -172,6 +159,7 @@ func (c *Connector) eventHandler(event string) (Callback, bool) {
 	return cb, ok
 }
 
+// responseHandler 获取响应处理器
 func (c *Connector) responseHandler(mid uint64) (Callback, bool) {
 	c.muResponses.RLock()
 	defer c.muResponses.RUnlock()
@@ -180,6 +168,7 @@ func (c *Connector) responseHandler(mid uint64) (Callback, bool) {
 	return cb, ok
 }
 
+// setResponseHandler 设置响应处理器
 func (c *Connector) setResponseHandler(mid uint64, cb Callback) {
 	c.muResponses.Lock()
 	defer c.muResponses.Unlock()
@@ -191,13 +180,12 @@ func (c *Connector) setResponseHandler(mid uint64, cb Callback) {
 	}
 }
 
+// sendMessage 发送消息
 func (c *Connector) sendMessage(msg *message.Message) error {
 	data, err := msg.Encode()
 	if err != nil {
 		return err
 	}
-
-	//log.Printf("%+v",msg)
 
 	payload, err := codec.Encode(packet.Data, data)
 	if err != nil {
@@ -210,6 +198,7 @@ func (c *Connector) sendMessage(msg *message.Message) error {
 	return nil
 }
 
+// write 写入数据到连接
 func (c *Connector) write() {
 	defer close(c.chSend)
 
@@ -227,10 +216,12 @@ func (c *Connector) write() {
 	}
 }
 
+// send 发送数据到发送队列
 func (c *Connector) send(data []byte) {
 	c.chSend <- data
 }
 
+// read 从连接读取数据
 func (c *Connector) read() {
 	buf := make([]byte, 2048)
 
@@ -256,6 +247,7 @@ func (c *Connector) read() {
 	}
 }
 
+// processPacket 处理数据包
 func (c *Connector) processPacket(p *packet.Packet) {
 	switch p.Type {
 	case packet.Handshake:
@@ -274,12 +266,13 @@ func (c *Connector) processPacket(p *packet.Packet) {
 	}
 }
 
+// processMessage 处理消息
 func (c *Connector) processMessage(msg *message.Message) {
 	switch msg.Type {
 	case message.Push:
 		cb, ok := c.eventHandler(msg.Route)
 		if !ok {
-			log.Println("event handler not found", msg.Route)
+			log.Println("未找到事件处理器", msg.Route)
 			return
 		}
 
@@ -288,7 +281,7 @@ func (c *Connector) processMessage(msg *message.Message) {
 	case message.Response:
 		cb, ok := c.responseHandler(msg.ID)
 		if !ok {
-			log.Println("response handler not found", msg.ID)
+			log.Println("未找到响应处理器", msg.ID)
 			return
 		}
 
@@ -297,6 +290,7 @@ func (c *Connector) processMessage(msg *message.Message) {
 	}
 }
 
+// serialize 序列化消息
 func serialize(v proto.Message) ([]byte, error) {
 	data, err := proto.Marshal(v)
 	if err != nil {
